b8edc9defd1b41e5316f0c37190e7cbccf33974b
Lim Byeong Chan <83913056+Sinbmil@users.noreply.github.com>
2022-03-14 00:15:51 +0900

    20220314

---
+++ 프로그래머스(Programmers)/Python/Lv1/문자열을 정수로 바꾸기.py
@@ -0,0 +1,13 @@
+# 프로그래머스 Lv1 - 문자열을 정수로 바꾸기
+# 문제링크: https://programmers.co.kr/learn/courses/30/lessons/12925
+
+# 정확성 테스트 케이스 15개 중 15개 성공
+# 총점 100.0
+
+def solution(s):
+    answer = int(s) # 정수형태로 강제 타입 변환
+    return answer
+
+s = 1234
+print (solution(s))
+# 출력 결과: 1234
