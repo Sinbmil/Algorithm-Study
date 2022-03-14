2bfd5f858b65bf625da754b27fd2c210c9a3a3ea
Lim Byeong Chan <83913056+Sinbmil@users.noreply.github.com>
2022-03-14 20:26:43 +0900

    20220314

--- 프로그래머스(Programmers)/Python/Lv1/제일 작은 수 제거하기.py
+++ 프로그래머스(Programmers)/Python/Lv1/제일 작은 수 제거하기.py
@@ -6,11 +6,11 @@
 
 def solution(arr):
     answer = []
-    if len(arr) != 1: # 배열의 길이가 1이 아닐 때
-        arr.remove(min(arr)) # 배열에서 최소값을 제거
-        answer = arr # arr배열을 answer로 대입
+    if len(arr) != 1: # 리스트의 길이가 1이 아닐 때
+        arr.remove(min(arr)) # 리스트에서 최소값을 제거
+        answer = arr # arr리스트를 answer로 대입
     else :
-        answer = [-1] # 배열의 길이가 1개일 경우 -1을 출력
+        answer = [-1] # 리스트의 길이가 1개일 경우 -1을 출력
     return answer
 
 arr = [4,3,2,1]
