37de56cd7c48bb22f89e6941f88c91d49111e6c7
Lim Byeong Chan <83913056+Sinbmil@users.noreply.github.com>
2022-03-29 21:29:00 +0900

    20220318

--- 프로그래머스(Programmers)/Python/Lv1/같은 숫자는 싫어.py
+++ 프로그래머스(Programmers)/Python/Lv1/같은 숫자는 싫어.py
@@ -2,7 +2,7 @@
 # 문제링크: https://programmers.co.kr/learn/courses/30/lessons/12906
 
 # 정확성 테스트 케이스 17개 중 17개 성공
-# 효율성 테스트 케이스 4개 중 4개 성공
+# 효율성 테스트 케이스  4개 중  4개 성공
 # 총점 100.0
 
 def solution(arr):
