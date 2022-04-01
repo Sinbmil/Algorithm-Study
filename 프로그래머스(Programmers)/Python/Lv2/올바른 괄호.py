9094575af4208b3d4f2c2e15c4410d333a71be67
Lim Byeong Chan <83913056+Sinbmil@users.noreply.github.com>
2022-04-02 00:40:02 +0900

    20220401

--- 프로그래머스(Programmers)/Python/Lv2/올바른 괄호.py
+++ 프로그래머스(Programmers)/Python/Lv2/올바른 괄호.py
@@ -12,14 +12,14 @@ def solution(s):
     for i in s:
         if i == '(':  # 현재 괄호가 '('이면 arr에 넣어주기
             arr.append(i) 
-        else: # 현재 괄호가 ')'이면
-            if arr == []: # 비어있으면 False 반환
-                answer = False
+        else: # 
+            if arr == []: # arr이 빈 스택이라면
+                answer = False # 올바르지 않은 괄호이므로 false를 반환하고 반복문 탈출
                 break
             else:
                 arr.pop() # arr의 맨끝에 괄호를 빼기
 
-    if arr != []: # 비어있지 않으면 False 반환
+    if arr != []: # 반복문 수행후 비어있지 않으면 False 반환
         answer = False
 
     return answer
