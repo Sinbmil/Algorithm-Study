6b7bad2c0c7df6bae678368a115d0aec91d2e080
Lim Byeong Chan <83913056+Sinbmil@users.noreply.github.com>
2022-04-04 00:26:53 +0900

    20220404

--- 백준(Baekjoon Online Judge)/Java/Silver/1978(소수 찾기).java
+++ 백준(Baekjoon Online Judge)/Java/Silver/1978(소수 찾기).java
@@ -4,21 +4,21 @@
 import java.util.*;
 public class Test {
 	public static void main(String[] args) {
-	      Scanner sc = new Scanner(System.in);
-	      int n = sc.nextInt(); // 숫자 개수
-	      int num;
-	      int count = 0;  // 소수 개수
-	      int i,j;
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt(); // 숫자 개수
+	    int num;
+	    int count = 0;  // 소수 개수
+	    int i,j;
 		
-	      for(i=0; i<n; i++){
-	          num = sc.nextInt();
-	          for(j=2; j<=1000; j++){
-		 if (num%j == 0)
-		      break;
-	           }
-	           if(num == j)
-		  count++; // 소수면 count 1증가
-	      }
-	     System.out.println(count);
+	    for(i=0; i<n; i++){
+	        num = sc.nextInt();
+	        for(j=2; j<=1000; j++){
+                    if(num%j == 0)
+		        break;
+	        }
+	        if(num == j)
+		    count++; // 소수면 count 1증가
+	    }
+	    System.out.println(count);
     }
 }
