097776233680af8468030aa54d657e32f5a95233
Lim Byeong Chan <83913056+Sinbmil@users.noreply.github.com>
2022-05-31 10:37:14 +0900

    20220531

--- 백준(Baekjoon Online Judge)/Java/Silver/1026(보물).java
+++ 백준(Baekjoon Online Judge)/Java/Silver/1026(보물).java
@@ -22,7 +22,7 @@ public class Test {
 		Arrays.sort(A); // A배열 정렬
 		Arrays.sort(B); // B배열 정렬
 		
-		for(int i=0; i<num; i++) { // sum에 A배열은 최소값부터 
+		for(int i=0; i<num; i++) {      // sum에 A배열은 최소값부터 
 			sum += A[i]*B[num-1-i]; // B배열은 최대값부터 곱해서 더하기
 		}
 		System.out.println(sum);
