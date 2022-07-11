313cb74c0204780ba9b1ca94b890d9def60ecd30
Lim Byeong Chan <qud902@naver.com>
2022-07-11 23:31:43 +0900

    20220711
    20220711

---
+++ 백준(Baekjoon Online Judge)/Java/Silver/1789(수들의 합).java
@@ -0,0 +1,21 @@
+// 백준 Silver5 - 1789(수들의 합)
+// 문제링크: https://www.acmicpc.net/problem/1789
+
+import java.util.*;
+
+public class Test {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long num = sc.nextLong();
+		long sum = 0;
+		int startNum = 0;
+		
+		for(int i=1; ; i++) {
+			if(sum > num)	
+				break;
+			sum += i;
+			startNum ++;
+		}
+		System.out.println(startNum-1);
+	}
+}
