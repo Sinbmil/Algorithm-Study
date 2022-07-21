4cca6cc7a150fd6ad2adf31357c12b20cdee05ae
Lim Byeong Chan <83913056+Sinbmil@users.noreply.github.com>
2022-07-21 20:33:32 +0900

    20220720

--- 프로그래머스(Programmers)/Java/Lv1/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스(Programmers)/Java/Lv1/로또의 최고 순위와 최저 순위.java
@@ -10,7 +10,7 @@ class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int answer[] = new int[2];
         int match = 0;  // 맞춘 번호
-		int zero = 0;   // 알아 볼 수 없는 번호
+	int zero = 0;   // 알아 볼 수 없는 번호
         
 		 for(int lotto : lottos){ // 번호를 알아볼 수 없는 경우
 			 if(lotto == 0){
