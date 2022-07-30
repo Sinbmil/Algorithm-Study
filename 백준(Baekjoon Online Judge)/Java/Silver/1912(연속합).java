0676cb26b3991a741620bc1528a3bd383daa5684
Lim Byeong Chan <qud902@naver.com>
2022-07-30 17:25:28 +0900

    20220730
    20220730

---
+++ 백준(Baekjoon Online Judge)/Java/Silver/1912(연속합).java
@@ -0,0 +1,33 @@
+// 백준 Silver2 - 1912(연속합)
+// 문제링크: https://www.acmicpc.net/problem/1912
+
+import java.util.*;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0;
+		int ans = 0;
+		int arr[] = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int i=0; i<n; i++) {
+			sum += arr[i];
+			if(sum<0)
+				sum = 0;
+			ans = Math.max(ans,sum);
+		}
+		
+		if(ans==0) { // 모든 원소가 음수인 경우
+			Arrays.sort(arr);
+			System.out.println(arr[arr.length-1]);
+		}else {
+			System.out.println(ans);
+		}
+	}
+}
