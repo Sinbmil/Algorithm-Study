02c4897817139a27ff407233aad9e076f563ee00
Lim Byeong Chan <qud902@naver.com>
2022-08-07 18:07:59 +0900

    20220807
    20220807

---
+++ 백준(Baekjoon Online Judge)/Java/Silver/1500(최대곱).java
@@ -0,0 +1,25 @@
+// 백준 Silver2 - 1500(최대곱)
+// 문제링크: https://www.acmicpc.net/problem/1500
+
+import java.util.*;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int S = sc.nextInt(); // 정수 S
+		int K = sc.nextInt(); // 합이 S인 K개의 정수
+		int div = S / K; 
+		int mod = S % K;
+		long max = 1;
+		for (int i=1; i<=K; i++) {
+			if(i<=mod) { // 나머지 갯수만큼 +1
+				max *= (div+1);
+			}else {
+				max *= div;
+			}
+		
+		}
+		System.out.println(max);
+	}	
+}
