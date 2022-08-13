0c67643be86866d390077e582e39c42fed70b4d8
Lim Byeong Chan <qud902@naver.com>
2022-08-13 14:30:33 +0900

    20220813
    20220813

---
+++ 백준(Baekjoon Online Judge)/Java/Silver/1427(소트인사이드).java
@@ -0,0 +1,25 @@
+// 백준 Silver5 - 1427(소트인사이드)
+// 문제링크: https://www.acmicpc.net/problem/1427
+
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		// int형 배열로 변환
+		int arr[] = Stream.of(String.valueOf(num).split("")).mapToInt(Integer::parseInt).toArray();
+		
+		// Integer형 배열로 변환
+		Integer[] arr2 = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+		
+		// 내림차순 정렬
+		Arrays.sort(arr2, Comparator.reverseOrder());
+		for (int i : arr2) {
+	        System.out.print(i + "");
+	    }
+    }
+}
