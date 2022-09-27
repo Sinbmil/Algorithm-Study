6368a132c5da1551d9cbf7c52cbb87ce332539a3
Lim Byeong Chan <83913056+Sinbmil@users.noreply.github.com>
2022-09-27 15:47:07 +0900

    20220927

--- 프로그래머스(Programmers)/Java/Lv2/이진 변환 반복하기.java
+++ 프로그래머스(Programmers)/Java/Lv2/이진 변환 반복하기.java
@@ -1,3 +1,10 @@
+/*
+ * 프로그래머스 Lv2 - 이진 변환 반복하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/70129
+ * 정확성 테스트 케이스 11개 중 11개 성공
+ * 총점 100.0
+*/
+
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[2];
